aed7b0559a3ba131a065c6ed471a3f75b46c35b9
Nikita Lobanov <nikitaalobanovv@gmail.com>
2024-11-15 00:04:05 -0800

    added comments

--- IOhandler.js
+++ IOhandler.js
@@ -16,14 +16,15 @@ const { EOL } = require('node:os');
 const unzip = async (pathIn, pathOut) => {
 	const zip = await yauzl.open(pathIn);
 	try {
-		await fs.promises.mkdir(pathOut, { recursive: true });
+		await fs.promises.mkdir(pathOut, { recursive: true }); //create the folder if it doesn't already exist
 
+		//loop through each file in the zip folder
 		for await (const entry of zip) {
-			const fileName = path.basename(entry.filename);
-			const outputPath = path.join(pathOut, fileName);
-			const readStream = await entry.openReadStream();
-			const writeStream = fs.createWriteStream(outputPath);
-			await pipeline(readStream, writeStream);
+			const fileName = path.basename(entry.filename); //get the input file name so you know what to name the output file
+			const outputPath = path.join(pathOut, fileName); //combine the file name with the output folder
+			const readStream = await entry.openReadStream(); //read each file
+			const writeStream = fs.createWriteStream(outputPath); //create a write stream for the output
+			await pipeline(readStream, writeStream); //get the data from the read file and create a new file in the unzipped folder
 			console.log(
 				`Extraction operation complete for ${entry.filename} at path ${outputPath}`
 			);
@@ -41,11 +42,11 @@ const unzip = async (pathIn, pathOut) => {
  */
 const readDir = async dir => {
 	try {
-		const files = await fsp.readdir(dir, { withFileTypes: true });
-		const pngFiles = files.map(file => path.join(dir, file.name));
-		return pngFiles;
+		//fsp = fs/promises
+		//reads each file in the folder, then joins the filename string with the directory string to get the full path to the file
+		return (await fsp.readdir(dir)).map(file => path.join(dir, file));
 	} catch (err) {
-		console.error(`error reading directory ${EOL} ${err}`);
+		console.log('error in readDir');
 		throw err;
 	}
 };
@@ -59,6 +60,7 @@ const readDir = async dir => {
  * @return {promise}
  */
 const grayScale = (pathIn, pathOut) => {
+	//everything in this function is from pngjs docs, just changed filter logic
 	fs.createReadStream(pathIn)
 		.pipe(
 			new PNG({
@@ -66,38 +68,25 @@ const grayScale = (pathIn, pathOut) => {
 			})
 		)
 		.on('parsed', function () {
-			for (let i = 0; i < this.data.length; i += 4) {
-				// gray = (red + green + blue)/3
-				const red = this.data[i];
-				const green = this.data[i + 1];
-				const blue = this.data[i + 2];
-				const gray = Math.round((red + green + blue) / 3); 
-				this.data[i] = gray; 
-				this.data[i + 1] = gray; 
-				this.data[i + 2] = gray; 
+			for (let y = 0; y < this.height; y++) {
+				for (let x = 0; x < this.width; x++) {
+					const idx = (this.width * y + x) << 2; 
+					// gray = (red + green + blue)/3
+					const red = this.data[idx];
+					const green = this.data[idx + 1];
+					const blue = this.data[idx + 2];
+					const gray = Math.round((red + green + blue) / 3);
+					this.data[idx] = gray;
+					this.data[idx + 1] = gray;
+					this.data[idx + 2] = gray;
+				}
 			}
-
 			this.pack().pipe(fs.createWriteStream(pathOut));
 		});
 };
 
-async function main() {
-	await unzip('./myfile.zip', './unzipped');
-	const directories = await readDir('./unzipped');
-	// directories is an array of filepaths, need to extract the names of each file to call the grayScale function with.
-	for (const filePath of directories) {
-		const fileName = path.basename(filePath);
-
-		const pathOut = path.join('./grayscaled', fileName);
-		grayScale(filePath, pathOut);
-		console.log(
-			`grayscale conversion complete for ${fileName} at ${pathOut}`
-		);
-	}
-}
-main();
-// module.exports = {
-// 	unzip,
-// 	readDir,
-// 	grayScale,
-// };
+module.exports = {
+	unzip,
+	readDir,
+	grayScale,
+};
